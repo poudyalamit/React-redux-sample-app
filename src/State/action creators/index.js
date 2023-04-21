c0067e3501be1a3616502900cc2d3a610214a483
poudyalamit <poudyalamit20@gmail.com>
2023-04-21 22:42:15 +0545

    made reducers

---
+++ src/State/action creators/index.js
@@ -0,0 +1,17 @@
+ const deposit =(amount)=>{
+    return (dispatch)=>{
+        dispatch({
+            type:'deposit',
+            payload:amount
+        })
+    }
+}
+export const withdraw =(amount)=>{
+    return (dispatch)=>{
+        dispatch({
+            type:'withdraw',
+            payload:amount
+        })
+    }
+}
+export default deposit;
